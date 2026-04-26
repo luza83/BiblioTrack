552b95e623b0269e65b583dd8af60e0de3b19d0f
luza83 <luz_ady83@yahoo.com.mx>
2026-04-26 18:29:25 +0200

    added health controller to ping app and avoid cold start

---
+++ BiblioTrack/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using BiblioTrack.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioTrack.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HealthController : Controller
+    {
+        private readonly ApiResponse _response;
+        public HealthController()
+        {
+          _response = new ApiResponse();      
+        }
+        [HttpGet]
+        public IActionResult Get() {
+           _response.IsSuccess = true;
+           _response.StatusCode = System.Net.HttpStatusCode.NoContent;
+           return Ok(_response);
+        }
+    }
+}
